5bab58d2507644f792fed8b0a0f62aceffd2828b
zaki.alloush <zaki.alloush@gmail.com>
2021-02-21 16:30:04 +0200

    Add Class DataConstants

---
+++ codersdcLibVideoPlayer/src/main/java/com/codersdc/codersdclibvideoplayer/DataConstants.java
@@ -0,0 +1,11 @@
+package com.codersdc.codersdclibvideoplayer;
+
+import com.google.android.exoplayer2.util.MimeTypes;
+
+public class DataConstants {
+    public static final String FROM_FRAGMENT_VIDEO_PLAYER = "from_fragment_video_player";
+    public static final String MIME_TYPE_DASH = MimeTypes.APPLICATION_MPD;
+    public static final String MIME_TYPE_HLS = MimeTypes.APPLICATION_M3U8;
+    public static final String MIME_TYPE_SS = MimeTypes.APPLICATION_SS;
+    public static final String MIME_TYPE_VIDEO_MP4 = MimeTypes.VIDEO_MP4;
+}
