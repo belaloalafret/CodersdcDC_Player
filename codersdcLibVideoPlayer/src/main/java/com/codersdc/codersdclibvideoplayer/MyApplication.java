23ddac53cd1ac03c78a73fa269143f3f8d813f38
zaki.alloush <zaki.alloush@gmail.com>
2021-03-25 15:23:37 +0200

    Update Manifest

--- codersdcLibVideoPlayer/src/main/java/com/codersdc/codersdclibvideoplayer/MyApplication.java
+++ codersdcLibVideoPlayer/src/main/java/com/codersdc/codersdclibvideoplayer/MyApplication.java
@@ -5,7 +5,28 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 
+
 public class MyApplication extends Application {
+
+    public static final String CHANNEL_ID_1 = "CHANNEL_1";
+    public static final String CHANNEL_ID_2 = "CHANNEL_2";
+    public static final String ACTION_NEXT = "NEXT";
+    public static final String ACTION_PREV = "PREVIOUS";
+    public static final String ACTION_PLAY = "PLAY";
+    public boolean inPipMode = false;
+
+
+    public static final boolean DEBUG_MODE = true;
+    private static MyApplication sInstance;
+
+    public static synchronized MyApplication getInstance() {
+        return sInstance;
+    }
+
+    public static Context getAppContext() {
+
+        return sInstance.getApplicationContext();
+    }
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -15,16 +36,18 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        createNotificationChannel();
+        sInstance = this;
+        //createNotificationChannel();
     }
 
-    private void createNotificationChannel() {
+    public void createNotificationChannel() {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel("CODERS_DC_CHANNEL_ID",
-                    "CodersDC Channel", NotificationManager.IMPORTANCE_DEFAULT);
-            notificationChannel.setDescription("CodersDC Channel Description");
+            NotificationChannel notificationChannel2 = new NotificationChannel(CHANNEL_ID_2,
+                    "Channel(2)", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel2.setDescription("Channel 2 Description");
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(notificationChannel);
+            notificationManager.createNotificationChannel(notificationChannel2);
         }
+
     }
 }
